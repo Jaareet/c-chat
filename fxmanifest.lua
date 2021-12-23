443cbc298e8cf868c308f0d2ada295842e2dea21
Jaareet <80985233+Jaareet@users.noreply.github.com>
2021-12-23 23:46:47 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+
+game        "common"
+
+author      "Jaareet#0097"
+
+dependency "chat"
+
+chat_theme "chat-design-v1" {
+    styleSheet = "chat-design-v1.css"
+}
+
+files {
+    "chat-design-v1.css",
+}
